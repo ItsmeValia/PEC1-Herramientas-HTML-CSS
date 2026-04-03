5ca5b381b1e7480cac5e7d0459d544843e5c25c5
María Rubio Martínez <mrubiomartinez1@uoc.edu>
2026-04-03 04:37:55 +0200

    estilos y botón top

--- src/app.js
+++ src/app.js
@@ -1,2 +1,37 @@
 const holi = 'world';
 console.log('Hello ' + holi);
+
+
+// SCROLL CON ANIMACIÓN
+
+document.getElementById('top-button').addEventListener('click', scrollTop);
+
+function scrollTop(){
+
+    var currentScroll = document.documentElement.scrollTop;
+    
+    if(currentScroll > 50){
+        window.requestAnimationFrame(scrollTop);
+        window.scrollTo(0, currentScroll - (currentScroll/8));
+    }
+}
+
+// BOTON QUE DESAPARECE
+
+buttonTop = document.getElementById('top-button');
+
+window.onscroll = function() {
+    var scroll = document.documentElement.scrollTop;
+
+    if(scroll > 200){
+        buttonTop.style.transform = "scale(1)";
+
+    }else{
+        buttonTop.style.transform = "scale(0)";
+    }
+}
+
+
+
+
+
